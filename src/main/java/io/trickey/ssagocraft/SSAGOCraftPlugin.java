845c8c86e190232aa93fc17f6a9ff071597786b0
Dan Trickey <dan@trickey.io>
2020-03-29 18:23:34 +0100

    Basic Bukkit Plugin

---
+++ src/main/java/io/trickey/ssagocraft/SSAGOCraftPlugin.java
@@ -0,0 +1,11 @@
+package io.trickey.ssagocraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SSAGOCraftPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello World!");
+    }
+}
