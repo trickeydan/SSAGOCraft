f738c56b01d9927e7ad9616972a26cff43a02a7f
Dan Trickey <dan@trickey.io>
2020-04-09 16:36:47 +0100

    Add CLI

---
+++ SSAGOCraftCLI/cli.py
@@ -0,0 +1,14 @@
+#! /opt/minecraft/ssagocraft/SSAGOCraftCLI/venv/bin/python3
+import os
+from sys import argv
+
+from mcrcon import MCRcon
+
+MC_SECRET = os.environ["SSAGOCRAFT_RCON_SECRET"]
+
+if len(argv) == 1:
+    command = input("Enter command: ")
+else:
+    command = " ".join(argv[1:])
+with MCRcon("127.0.0.1", MC_SECRET) as mcr:
+    print(mcr.command(command))
