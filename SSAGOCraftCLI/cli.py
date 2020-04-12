9bd5fed9d4d1b17bd771037b14d46764f1a03b85
Dan Trickey <dan@trickey.io>
2020-04-12 17:57:50 +0100

    Support multiple servers

--- SSAGOCraftCLI/cli.py
+++ SSAGOCraftCLI/cli.py
@@ -2,13 +2,20 @@
 import os
 from sys import argv
 
+import click
 from mcrcon import MCRcon
 
 MC_SECRET = os.environ["SSAGOCRAFT_RCON_SECRET"]
+SERVERS = {"vanilla": ("127.0.0.1", 25575)}
 
-if len(argv) == 1:
-    command = input("Enter command: ")
-else:
-    command = " ".join(argv[1:])
-with MCRcon("127.0.0.1", MC_SECRET) as mcr:
-    print(mcr.command(command))
+@click.command()
+@click.option('--server', default='vanilla', help='Name of Server', type=click.Choice(SERVERS.keys()))
+@click.argument('command')
+def execute(server, command):
+    info = SERVERS[server]
+    with MCRcon(info[0], MC_SECRET, port=info[1]) as mcr:
+        print(mcr.command(command))
+if __name__ == '__main__':
+    execute()
+
+
