8c79fb087f9c3a918986b4f784c35c055bec5fd9
Dan Trickey <dan@trickey.io>
2020-04-13 17:23:20 +0100

    Add basic Bungeecord plugin

---
+++ SSAGOCraftProxy/src/main/java/io/trickey/SSAGOCraftProxy/SSAGOCraftProxyPlugin.java
@@ -0,0 +1,13 @@
+package io.trickey.SSAGOCraftProxy;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class SSAGOCraftProxyPlugin extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // You should not put an enable message in your plugin.
+        // BungeeCord already does so
+        getLogger().info("Yay! It loads!");
+    }
+}
