04d200ecb01a8e03e5b58ce8fee151054f9d4f96
Dan Trickey <dan@trickey.io>
2020-04-13 19:56:21 +0100

    Query the web server to see if a player is whitelisted

--- SSAGOCraftProxy/src/main/java/io/trickey/SSAGOCraftProxy/LoginListener.java
+++ SSAGOCraftProxy/src/main/java/io/trickey/SSAGOCraftProxy/LoginListener.java
@@ -1,5 +1,6 @@
 package io.trickey.SSAGOCraftProxy;
 
+import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.PendingConnection;
@@ -7,6 +8,8 @@ import net.md_5.bungee.api.event.LoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
+
+import java.io.IOException;
 
 public class LoginListener implements Listener {
 
@@ -19,21 +22,34 @@ public class LoginListener implements Listener {
     }
 
     @EventHandler
-    public void onNetworkJoin(LoginEvent e) {
-        PendingConnection p = e.getConnection();
+    public void onNetworkJoin(LoginEvent event) {
+        PendingConnection p = event.getConnection();
         plugin.getLogger().info("Checking whitelist status of " + p.getName() + "(" + p.getUniqueId() + ")");
-        if(!wc.isWhitelisted(p)){
-            plugin.getLogger().info(p.getName() + "(" + p.getUniqueId() + ") was not found on the whitelist.");
-            disconnect(p);
-        } else {
-            plugin.getLogger().info(p.getName() + " was found on the whitelist.");
+        try {
+            if (!wc.isWhitelisted(p)) {
+                plugin.getLogger().info(p.getName() + "(" + p.getUniqueId() + ") was not found on the whitelist.");
+                disconnect(p);
+            } else {
+                plugin.getLogger().info(p.getName() + " was found on the whitelist.");
+            }
+        } catch(IOException e){
+            plugin.getLogger().severe("Whitelist Error: " + e.toString());
+            disconnect(p, "There was an error whilst joining. Please contact the Minecraft Team.");
         }
+    }
+
+    public void disconnect(PendingConnection p, String message){
+        disconnect(p, new TextComponent(message));
+    }
+
+    public void disconnect(PendingConnection p, BaseComponent message){
+        plugin.getLogger().info("Disconnecting " + p.getName());
+        p.disconnect(message);
     }
 
     public void disconnect(PendingConnection p){
         TextComponent message = new TextComponent("Please register your Minecraft Account at https://minecraft.ssago.org");
         message.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://minecraft.ssago.org/"));
-        plugin.getLogger().info("Disconnecting " + p.getName());
-        p.disconnect(message);
+        disconnect(p, message);
     }
 }
--- SSAGOCraftProxy/src/main/java/io/trickey/SSAGOCraftProxy/WhitelistChecker.java
+++ SSAGOCraftProxy/src/main/java/io/trickey/SSAGOCraftProxy/WhitelistChecker.java
@@ -3,6 +3,8 @@ package io.trickey.SSAGOCraftProxy;
 import net.md_5.bungee.api.connection.PendingConnection;
 import net.md_5.bungee.api.plugin.Plugin;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -13,14 +15,26 @@ public class WhitelistChecker {
 
     public WhitelistChecker(Plugin plugin){
         this.plugin = plugin;
-        try {
-            endpoint = new URL("https://minecraft.ssago.org/");
-        } catch(MalformedURLException e){
-            plugin.getLogger().severe("Bad URL: " + e.toString());
-        }
     }
 
-    public boolean isWhitelisted(PendingConnection p){
-        return false;
+    public boolean isWhitelisted(PendingConnection p) throws IOException {
+        try {
+            endpoint = new URL("https://minecraft.ssago.org/api/check?uuid=" + p.getUniqueId().toString());
+        } catch (MalformedURLException e) {
+            plugin.getLogger().severe("Bad URL: " + e.toString());
+        }
+        HttpURLConnection con = (HttpURLConnection) endpoint.openConnection();
+        con.setRequestMethod("GET");
+        con.setConnectTimeout(500);
+        con.setReadTimeout(500);
+        int status = con.getResponseCode();
+        switch(status){
+            case 200:
+                return true;
+            case 401:
+                return false;
+            default:
+                throw new IOException("Received error code " + Integer.toString(status));
+        }
     }
 }
