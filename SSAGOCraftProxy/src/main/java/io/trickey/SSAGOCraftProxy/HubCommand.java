61ea849e47f0d9f251f5a1ecc9a249e812d90fc6
Dan Trickey <dan@trickey.io>
2020-05-28 14:27:45 +0100

    Add hub command

---
+++ SSAGOCraftProxy/src/main/java/io/trickey/SSAGOCraftProxy/HubCommand.java
@@ -0,0 +1,35 @@
+package io.trickey.SSAGOCraftProxy;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Command;
+
+public class HubCommand extends Command {
+
+    private final ServerInfo target;
+
+    public HubCommand(){
+        super("hub");
+        target = ProxyServer.getInstance().getServerInfo("hub");
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] strings) {
+        if (!(sender instanceof ProxiedPlayer)) {
+            sender.sendMessage(new ComponentBuilder("This command can only be run by a player!").color(ChatColor.RED).create());
+            return;
+        }
+        ProxiedPlayer player = (ProxiedPlayer) sender;
+
+        if (player.getServer().getInfo().getName().equalsIgnoreCase("hub")) {
+            player.sendMessage(new ComponentBuilder("You are already connected to the Hub!").color(ChatColor.RED).create());
+            return;
+        }
+        player.connect(target);
+        player.sendMessage(new ComponentBuilder("Welcome to the Hub!").color(ChatColor.GREEN).create());
+    }
+}
--- SSAGOCraftProxy/src/main/java/io/trickey/SSAGOCraftProxy/SSAGOCraftProxyPlugin.java
+++ SSAGOCraftProxy/src/main/java/io/trickey/SSAGOCraftProxy/SSAGOCraftProxyPlugin.java
@@ -1,12 +1,15 @@
 package io.trickey.SSAGOCraftProxy;
 
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.api.plugin.PluginManager;
 
 public class SSAGOCraftProxyPlugin extends Plugin {
 
     @Override
     public void onEnable() {
-        getProxy().getPluginManager().registerListener(this, new LoginListener(this));
-        getProxy().getPluginManager().registerListener(this, new KickListener(this));
+        PluginManager pm = getProxy().getPluginManager();
+        pm.registerListener(this, new LoginListener(this));
+        pm.registerListener(this, new KickListener(this));
+        pm.registerCommand(this, new HubCommand());
     }
 }
